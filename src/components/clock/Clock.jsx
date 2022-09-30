c2830cfca697b00c68efdca0d1c2df2e6c6d5ffb
LuckyJVIan <territorial63@gmail.com>
2022-09-30 22:18:33 +0400

    feature/MN-08: edits fpr PR

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./clock.scss";
 
-export const Clock = ({ sec = 2000 }) => {
+export const Clock = ({ sec = 1000 }) => {
   const [date, setDate] = useState(new Date());
 
-  const clockInterval = setInterval(() => setDate(new Date()), sec);
-
   useEffect(() => {
+    const clockInterval = setInterval(() => setDate(new Date()), sec);
     return () => clearInterval(clockInterval);
   }, []);
-
+  
   return (
     <div className="clock">
       <span>{date.toLocaleTimeString()}</span>
